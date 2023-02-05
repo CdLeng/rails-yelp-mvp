df78a37ec9fcea5eb95c0ed1cfbf32569e95c903
Coen de Leng <coendeleng@outlook.com>
2023-02-05 17:10:42 +0100

    Simple Yelp copycat

---
+++ app/models/restaurant.rb
@@ -0,0 +1,13 @@
+class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+
+  validates :name, :address, :category, presence: true
+  validates :category, inclusion: { in: %w[chinese italian japanese french belgian],
+  message: "%{value} is not a valid category" }
+
+  before_validation :downcase
+
+  def downcase
+    self.category.downcase!
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
+  resources :restaurants do
+    resources :reviews, only: [:new, :create]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get 'restaurants', to: 'restaurants#index'
+  get 'restaurants/new', to: 'restaurants#new', as: :create_restaurant
+  post 'restaurants', to: 'restaurants#create'
+  get 'restaurants/:id', to: 'restaurants#show', as: :show_restaurant
+  get 'restaurants/:id/reviews/new', to: 'reviews#new', as: :create_review
+  post 'restaurants/:id/reviews', to: 'reviews#create', as: :review
 end
